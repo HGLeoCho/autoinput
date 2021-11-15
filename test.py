9e163f9f5e41ccef333297e8a810348a9462d37a
HoGyeong Leo Cho <chg8778@gmail.com>
2021-11-14 23:43:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,29 @@
-class LineNumber:
+class PrimarySIM:
   def __init__(self, VoLTE_able, number, CID):
     self.VoLTE_able = VoLTE_able
     self.number = number
     self.CID = CID
    
   def print_attribute(self):
-    return str(self.number) + " is " + str(self.VoLTE_able) + ", " + self.CID
+    print(str(self.number) + " is " + str(self.VoLTE_able) + ", " + self.CID)
   
   
+class CarrierA(PrimarySIM):
+  def sound(self):
+    print("AAA")
+    
+class CarrierB(PrimarySIM):
+  def sound(self):
+    print("BBB")
   
 # ================
 
-EXECUTION
+# EXECUTION
 
 # ================
 
-numb1 = LineNumber("TRUE", "4169999999", "AAA")
+numb1 = PrimarySIM("TRUE", "4169999999", "AAA")
 numb1.print_attribute()
+
+numb2 = CarrierA("FALSE", "6471111111", "AAA")
+numb2.sound()
