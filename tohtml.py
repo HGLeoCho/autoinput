99168ca46b7fa1ecf3bc972a3885af167882ce0e
HoGyeong Leo Cho <chg8778@gmail.com>
2021-11-20 13:31:44 -0500

    Update tohtml.py

--- tohtml.py
+++ tohtml.py
@@ -31,5 +31,26 @@ def filter_IMS(*args):
       confirm_string = line.rstrip()
       temp_number = 0
       for m in range(len(filter_short)):
-        if filter_short[m] in confirm_string:
-          tolerence_count = 0
+        if filter_short[m] not in confirm_string:         
+          tolerence_count = 0          
+        else:
+          temp_number += 1
+          if temp_index == m:
+            temp_string += '\n' + confirm_string
+          elif temp_string == '':
+            temp_string = confirm_string
+            temp_index = m
+          else:
+            result[temp_index].append(temp_string)
+            temp_string = confirm_string
+            temp_index = m
+          
+      if temp_number == len(filter_short):
+        tolerence_count += 1
+      if tolerence_count == 2 and temp_string != '':
+        result[temp_index].append(temp_string)
+        temp_string = ''
+        temp_index = -1
+        tolerence_count = 0
+      if tolerence_count == 2:
+        tolerence_count = 0
